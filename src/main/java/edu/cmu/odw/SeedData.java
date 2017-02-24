830d2c5ca689217739f91a555c1af603924c45c1
ubuntu <ubuntu@ip-172-31-62-165.ec2.internal>
2017-02-24 22:33:33 +0000

    Added comment data

--- src/main/java/edu/cmu/odw/SeedData.java
+++ src/main/java/edu/cmu/odw/SeedData.java
@@ -23,10 +23,39 @@ public class SeedData {
     @Bean
     public SeedData getBean() {        
 
-        Comment c1 = new Comment("text",new Long(1),new Timestamp(new Long("1486902720000")),"123.214.242.122");
-        
+        Comment c1 = new Comment("Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",new Long(1),new Timestamp(new Long("1486902720000")),"123.214.242.122");
+        Comment c2 = new Comment("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio.",new Long(1),new Timestamp(new Long("1491491601000")),"211.169.248.159");
+        Comment c3 = new Comment("Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat.",new Long(1),new Timestamp(new Long("1487002503000")),"218.40.141.100");
+        Comment c4 = new Comment("Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",new Long(1),new Timestamp(new Long("1488713309000")),"211.25.189.42");
+        Comment c5 = new Comment("Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",new Long(2),new Timestamp(new Long("1489897172000")),"218.40.141.100");
+        Comment c6 = new Comment("Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros.",new Long(2),new Timestamp(new Long("1493157436000")),"211.25.189.42");
+        Comment c7 = new Comment("Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",new Long(2),new Timestamp(new Long("1487909165000")),"218.40.141.100");
+        Comment c8 = new Comment("Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor.",new Long(2),new Timestamp(new Long("1490487323000")),"108.43.182.211");
+        Comment c9 = new Comment("Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",new Long(3),new Timestamp(new Long("1491804374000")),"102.141.148.237");
+        Comment c10 = new Comment("Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros.",new Long(3),new Timestamp(new Long("1487373586000")),"140.251.245.73");
+        Comment c11 = new Comment("Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",new Long(3),new Timestamp(new Long("1486654011000")),"140.251.245.73");
+        Comment c12 = new Comment("Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti.",new Long(3),new Timestamp(new Long("1484536047000")),"35.111.167.21");
+        Comment c13 = new Comment("Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor.",new Long(3),new Timestamp(new Long("1492904055000")),"149.36.240.218");
+        Comment c14 = new Comment("Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros.",new Long(4),new Timestamp(new Long("1488881121000")),"149.36.240.218");
+        Comment c15 = new Comment("Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",new Long(4),new Timestamp(new Long("1489434147000")),"109.120.19.198");
+        Comment c16 = new Comment("Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor.",new Long(5),new Timestamp(new Long("1491523121000")),"207.231.251.35");
         
         commentService.save(c1);
+        commentService.save(c2);
+        commentService.save(c3);
+        commentService.save(c4);
+        commentService.save(c5);
+        commentService.save(c6);
+        commentService.save(c7);
+        commentService.save(c8);
+        commentService.save(c9);
+        commentService.save(c10);
+        commentService.save(c11);
+        commentService.save(c12);
+        commentService.save(c13);
+        commentService.save(c14);
+        commentService.save(c15);
+        commentService.save(c16);
         
         log.info("Comments found with findAll():");
         log.info("-------------------------------");
